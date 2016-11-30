c2bfbef2927fbc98d04da2736189b0d037332215
marek_n <mareknenal@gmail.com>
2016-11-30 23:04:04 +0100

    [#14] Test (save) sales_invoice_save method in DB

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -7,9 +7,18 @@ RSpec.describe Database do
       data.transaction do
         data[:invoices_data_index] = [[1, 'invoice sales', 123]]
       end
+
       database = Database.sales_invoice
 
       expect(database).to eq [[1, 'invoice sales', 123]]
+      File.open('./db/database_sales_invoice', 'w') {|file| file.truncate(0) }
+    end
+
+    it '#sales_invoice_save' do
+      database = Database.sales_invoice_save([[2, 'invoice sales', 345]])
+
+      expect(database).to eq [[2, 'invoice sales', 345]]
+      File.open('./db/database_sales_invoice', 'w') {|file| file.truncate(0) }
     end
   end
 end
