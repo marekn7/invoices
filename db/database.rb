b611f78c170ce6ea658aaa9d986efb1896fe114d
marek_n <mareknenal@gmail.com>
2016-11-30 23:53:23 +0100

    [#15] Refactoring database.rb file, add tests for purchase invoice

--- db/database.rb
+++ db/database.rb
@@ -3,56 +3,56 @@ require "pstore"
 class Database
 
   def self.sales_invoice
-    invoices_data = PStore.new("./db/database_sales_invoice")
+    invoices_data = PStore.new("./db/database")
     invoices_data.transaction(true) do
-      invoices_data[:invoices_data_index] || []
+      invoices_data[:sales_invoice_data_index] || []
     end
   end
 
   def self.sales_invoice_save(invoices)
-    invoices_data = PStore.new("./db/database_sales_invoice")
+    invoices_data = PStore.new("./db/database")
     invoices_data.transaction do
-      invoices_data[:invoices_data_index] = invoices
+      invoices_data[:sales_invoice_data_index] = invoices
     end
   end
 
   def self.purchase_invoice
-    invoices_data = PStore.new("./db/database_purchase_invoice")
+    invoices_data = PStore.new("./db/database")
     invoices_data.transaction(true) do
-      invoices_data[:invoices_data_index] || []
+      invoices_data[:purchase_invoice_data_index] || []
     end
   end
 
   def self.purchase_invoice_save(invoices)
-    invoices_data = PStore.new("./db/database_purchase_invoice")
+    invoices_data = PStore.new("./db/database")
     invoices_data.transaction do
-      invoices_data[:invoices_data_index] = invoices
+      invoices_data[:purchase_invoice_data_index] = invoices
     end
   end
 
   def self.contacts
-    contacts_data = PStore.new("./db/database_contact")
+    contacts_data = PStore.new("./db/database")
     contacts_data.transaction(true) do
       contacts_data[:contacts_data_index] || []
     end
   end
 
   def self.contact_save(contacts)
-    contacts_data = PStore.new("./db/database_contact")
+    contacts_data = PStore.new("./db/database")
     contacts_data.transaction do
       contacts_data[:contacts_data_index] = contacts
     end
   end
 
   def self.items
-    items_data = PStore.new("./db/database_item")
+    items_data = PStore.new("./db/database")
     items_data.transaction(true) do
       items_data[:items_data_index] || []
     end
   end
 
   def self.item_save(items)
-    items_data = PStore.new("./db/database_item")
+    items_data = PStore.new("./db/database")
     items_data.transaction do
       items_data[:items_data_index] = items
     end
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -1,24 +1,43 @@
 require "./db/database"
 
 RSpec.describe Database do
-  describe 'invoice database' do
+  describe 'read and save from database' do
     it '#sales_invoice' do
-      data = PStore.new("./db/database_sales_invoice")
+      data = PStore.new("./db/database")
       data.transaction do
-        data[:invoices_data_index] = [[1, 'invoice sales', 123]]
+        data[:sales_invoice_data_index] = [[1, 'invoice sales', 123]]
       end
 
       database = Database.sales_invoice
 
       expect(database).to eq [[1, 'invoice sales', 123]]
-      File.open('./db/database_sales_invoice', 'w') {|file| file.truncate(0) }
+      File.open('./db/database', 'w') {|file| file.truncate(0) }
     end
 
     it '#sales_invoice_save' do
       database = Database.sales_invoice_save([[2, 'invoice sales', 345]])
 
       expect(database).to eq [[2, 'invoice sales', 345]]
-      File.open('./db/database_sales_invoice', 'w') {|file| file.truncate(0) }
+      File.open('./db/database', 'w') {|file| file.truncate(0) }
+    end
+
+    it '#purchase_invoice' do
+      data = PStore.new("./db/database")
+      data.transaction do
+        data[:purchase_invoice_data_index] = [[1, 'purchase sales', 123]]
+      end
+
+      database = Database.purchase_invoice
+
+      expect(database).to eq [[1, 'purchase sales', 123]]
+      File.open('./db/database', 'w') {|file| file.truncate(0) }
+    end
+
+    it '#purchase_invoice_save' do
+      database = Database.purchase_invoice_save([1, 'purchase sales', 456])
+
+      expect(database).to eq [1, 'purchase sales', 456]
+      File.open('./db/database', 'w') {|file| file.truncate(0) }
     end
   end
 end
